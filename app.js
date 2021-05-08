ce4eb9f100eb609786c89c6880fe59626853318a
MarketingPlus <benmarketingplus@gmail.com>
2021-05-08 16:53:39 +1000

    added a const so we can target the file

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 const emailValidator = require('email-validator');
+
+const OUTPUT_DIR = path.resolve(__dirname, "final");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
